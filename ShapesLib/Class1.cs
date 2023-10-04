c88d7d0edafb182d8f71e2f17ef975bfd22d8b90
yark0stalo <yark0.stalo@gmail.com>
2023-10-04 14:31:31 +0300

    - create shapes library

---
+++ ShapesLib/Class1.cs
@@ -0,0 +1,50 @@
+﻿namespace ShapesLibrary
+{
+    public interface IShape
+    {
+        double CalculateArea();
+    }
+
+    public class Circle : IShape
+    {
+        private double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        private double Side1 { get; set; }
+        private double Side2 { get; set; }
+        private double Side3 { get; set; }
+
+        public Triangle(double side1, double side2, double side3)
+        {
+            Side1 = side1;
+            Side2 = side2;
+            Side3 = side3;
+        }
+
+        public double CalculateArea()
+        {
+            double s = (Side1 + Side2 + Side3) / 2;
+            return Math.Sqrt(s * (s - Side1) * (s - Side2) * (s - Side3));
+        }
+
+        public bool IsRightTriangle()
+        {
+            double[] sides = { Side1, Side2, Side3 };
+            Array.Sort(sides);
+
+            return Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2);
+        }
+    }
+}
